9e9f9b942c3c3a74a745fe4529dc7b790198c062
Felipe Soller <soller@outlook.com>
2020-10-26 15:05:05 -0300

    Refatoração de código utilizando Nunjucks

---
+++ backend/data.js
@@ -0,0 +1,38 @@
+module.exports = [
+    {
+        id: "GykTLqODQuU",
+        title: "Formulário Animado com JS puro e CSS Animation | Mayk Brito",
+        duration: "57 Minutos",
+        price: "free"
+    },
+    {
+        id: "HN1UjzRSdBk",
+        title: "Desvendando o CSS Grid na prática | Mayk Brito",
+        duration: "36 Minutos",
+        price: "free"
+    },
+    {
+        id: "rAzHvYnQ8DY",
+        title: "Array: High order Functions | Mayk Brito",
+        duration: "54 Minutos",
+        price: "free"
+    },
+    {
+        id: "ghTrp1x_1As",
+        title: "O que é API? REST e RESTful? | Mayk Brito",
+        duration: "33 Minutos",
+        price: "free"
+    },
+    {
+        id: "GSqR2i-Pq6o",
+        title: "Desvendando a variável THIS | Mayk Brito",
+        duration: "48 Minutos",
+        price: "free"
+    },
+    {
+        id: "2alg7MQ6_sI",
+        title: "Git e GitHub na prática | Mayk Brito",
+        duration: "33 Minutos",
+        price: "free"
+    },
+]
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const nunjucks = require('nunjucks');
 
 const server = express();
+const videos = require('./data');
 
 server.use(express.static('public'));
 
@@ -12,11 +13,19 @@ nunjucks.configure('views', {
 });
 
 server.get('/', function(require, response) {
-    return response.render('about');
+    const about = {
+        avatar_url: "https://avatars0.githubusercontent.com/u/62728038?s=400&u=0901764d6f0e877dd57621cc0b6aa65d399a9ffb&v=4",
+        name: "Felipe Soller",
+        role: "Desenvolvedor Web Júnior",
+        description: "Engenheiro Mecânico migrando para área de Desenvolvimento Web Front End criando aplicativos web visualmente atrativos e de fácil usabilidade, utilizando HTML, CSS, JavaScript e Node.JS. Habilidade técnica em estilização e design de aplicativos web, criação de sites interativos, desenvolvimento back-end e desenvolvimento orientado a testes com JavaScript, criação de aplicativos front-end com React, SQL e bancos de dados para desenvolvimento web, criação de API. Capacidade de trabalhar sob pressão, prazer em aprender novas habilidades, pensamento crítico, criativo e resolutivo em problemas, rápida adaptação a mudanças, boa comunicação como emissor e ouvinte, responsável em todas as tarefas e gestão de tempo. Atualmente cursando o Bootcamp LaunchBase da Rocketseat.",
+
+    }
+
+    return response.render('about', { about });
 });
 
 server.get('/courses', function(require, response) {
-    return response.render('courses');
+    return response.render('courses', { items: videos });
 });
 
 

a82396e5a52dad5adc267f4b1491233d5dabcf2e
Felipe Soller <soller@outlook.com>
2020-10-26 00:22:54 -0300

    Instalação e configuração do template engine Nunjucks

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,24 @@
 const express = require('express');
+const nunjucks = require('nunjucks');
+
 const server = express();
 
+server.use(express.static('public'));
+
+server.set('view engine', 'njk');
+
+nunjucks.configure('views', {
+    express:server
+});
+
 server.get('/', function(require, response) {
-    return response.send('Hello World!!!')
-})
+    return response.render('about');
+});
+
+server.get('/courses', function(require, response) {
+    return response.render('courses');
+});
+
 
 server.listen(3000, function() {
     console.log('server is running');

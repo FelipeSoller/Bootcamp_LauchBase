c118c92714d443fe50d7e9952c81e4950283d742
Felipe Soller <soller@outlook.com>
2020-10-26 15:20:08 -0300

    Adição da indicação de videos em destaque

--- backend/data.js
+++ backend/data.js
@@ -3,7 +3,8 @@ module.exports = [
         id: "GykTLqODQuU",
         title: "Formulário Animado com JS puro e CSS Animation | Mayk Brito",
         duration: "57 Minutos",
-        price: "free"
+        price: "free",
+        featured: true,
     },
     {
         id: "HN1UjzRSdBk",
@@ -15,13 +16,15 @@ module.exports = [
         id: "rAzHvYnQ8DY",
         title: "Array: High order Functions | Mayk Brito",
         duration: "54 Minutos",
-        price: "free"
+        price: "free",
+        featured: true,
     },
     {
         id: "ghTrp1x_1As",
         title: "O que é API? REST e RESTful? | Mayk Brito",
         duration: "33 Minutos",
-        price: "free"
+        price: "free",
+        featured: true,
     },
     {
         id: "GSqR2i-Pq6o",
